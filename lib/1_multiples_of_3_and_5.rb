fcf9448e4ff7467e138df11c501a545396f76ad1
Derek Maffett <derekgmaffett@gmail.com>
2014-10-11 12:38:54 -0700

    remove inferior solutions

--- lib/1_multiples_of_3_and_5.rb
+++ lib/1_multiples_of_3_and_5.rb
@@ -7,21 +7,6 @@ class MultiplesSum
     @criteria_array = criteria_array
   end
 
-  # This would appear to be the fastest method
-  # def analyze(num)
-  #   result = 0
-  #   (1...num).each do |i|
-      # result += i if divisible_by_criteria?(i)
-  #   end
-  #   result
-  # end
-
-  # This solution looks nicer, but returns an unnecessary array with the
-  # select method.
-  # def analyze(num)
-    # (1...num).select { |i| divisible_by_criteria?(i) }.reduce(&:+)
-  # end
-
   def analyze(num)
     (1...num).inject(0) { |sum, i| divisible_by_criteria?(i) ? sum + i : sum }
   end
