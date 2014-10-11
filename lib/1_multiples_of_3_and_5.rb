681d9497ca44e82595f23e9ab6663027c1b51c98
Derek Maffett <derekgmaffett@gmail.com>
2014-10-11 12:37:24 -0700

    refactor solutions for problem one

--- lib/1_multiples_of_3_and_5.rb
+++ lib/1_multiples_of_3_and_5.rb
@@ -1,16 +1,32 @@
+# This algorithm returns the sum of numbers below an input number x which are
+# divisible by 3 and 5. The solution accepts other arrays of divisibility
+# matchers.
+
 class MultiplesSum
-  def initialize(criteria_hash)
+  def initialize(criteria_array)
     @criteria_array = criteria_array
   end
 
+  # This would appear to be the fastest method
+  # def analyze(num)
+  #   result = 0
+  #   (1...num).each do |i|
+      # result += i if divisible_by_criteria?(i)
+  #   end
+  #   result
+  # end
+
+  # This solution looks nicer, but returns an unnecessary array with the
+  # select method.
+  # def analyze(num)
+    # (1...num).select { |i| divisible_by_criteria?(i) }.reduce(&:+)
+  # end
+
   def analyze(num)
-    result = 0
-    (1..num).each do |i|
-      result += i if divisible_by_criteria(i)
-    end
+    (1...num).inject(0) { |sum, i| divisible_by_criteria?(i) ? sum + i : sum }
   end
 
-  def divisible_by_criteria(i)
+  def divisible_by_criteria?(i)
     @criteria_array.each do |crit|
       return true if i % crit == 0
     end
--- spec/1_multiples_of_3_and_5_spec.rb
+++ spec/1_multiples_of_3_and_5_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 require '1_multiples_of_3_and_5'
 
 describe MultiplesSum do
+  it 'should return appropriate sum for 5' do
+    MultiplesSum.new([3, 5]).analyze(5).must_equal 3
+  end
+
   it 'should return appropriate sum for 10' do
     MultiplesSum.new([3, 5]).analyze(10).must_equal 23
   end
 
   it 'should return appropriate sum for 15' do
-    MultiplesSum.new([3, 5]).analyze(15).must_equal 50
+    MultiplesSum.new([3, 5]).analyze(15).must_equal 45
   end
 end
